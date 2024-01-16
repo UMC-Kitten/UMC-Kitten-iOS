d00dc5b68cb4dfb8e6a6a8b93592ef5aa32e2bcd
leedoyeon849 <do83430208@gmail.com>
2024-01-17 00:03:57 +0900

    feat: Font-Pretendard 파일 추가 및 UILabel Extension 추가

--- UMC-Kitten-iOS/ViewController.swift
+++ UMC-Kitten-iOS/ViewController.swift
@@ -11,7 +11,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // UILabel 생성 및 설정
+        let label = UILabel()
+        label.setDefaultFont(weight: .black)
+        label.setDefaultFont(size: 20, weight: .black)
+        label.text = "안녕하세요, iOS 개발!"
+        label.textAlignment = .center
+        label.textColor = .black
+        
+        // 레이블을 뷰에 추가
+        view.addSubview(label)
+        
+        // 레이블의 오토레이아웃 설정
+        label.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
 
 
