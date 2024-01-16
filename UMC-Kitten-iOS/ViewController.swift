33ba87492f04252e453d9bdcac1a3c1a16762d33
leedoyeon849 <do83430208@gmail.com>
2024-01-17 00:10:57 +0900

    delete: 메인 VC ViewDidLoad color/font 테스트 코드 삭제

--- UMC-Kitten-iOS/ViewController.swift
+++ UMC-Kitten-iOS/ViewController.swift
@@ -11,27 +11,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // UILabel 생성 및 설정
-        let label = UILabel()
-        // 폰트 설정 법
-        label.setDefaultFont(weight: .black)
-        label.setDefaultFont(size: 20, weight: .black)
-        // 컬러 사용 법
-        label.textColor = .grayScale400
-        label.textColor = .mainDeep
-        label.text = "안녕하세요, iOS 개발!"
-        label.textAlignment = .center
-        
-        // 레이블을 뷰에 추가
-        view.addSubview(label)
-        
-        // 레이블의 오토레이아웃 설정
-        label.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
     }
 
 
