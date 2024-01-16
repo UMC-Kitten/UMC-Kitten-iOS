004d05492daee2fc142482b309675793d2d7ec15
leedoyeon849 <do83430208@gmail.com>
2024-01-17 00:10:19 +0900

    feat: Asset Color 추가

--- UMC-Kitten-iOS/ViewController.swift
+++ UMC-Kitten-iOS/ViewController.swift
@@ -14,11 +14,14 @@ class ViewController: UIViewController {
         
         // UILabel 생성 및 설정
         let label = UILabel()
+        // 폰트 설정 법
         label.setDefaultFont(weight: .black)
         label.setDefaultFont(size: 20, weight: .black)
+        // 컬러 사용 법
+        label.textColor = .grayScale400
+        label.textColor = .mainDeep
         label.text = "안녕하세요, iOS 개발!"
         label.textAlignment = .center
-        label.textColor = .black
         
         // 레이블을 뷰에 추가
         view.addSubview(label)
