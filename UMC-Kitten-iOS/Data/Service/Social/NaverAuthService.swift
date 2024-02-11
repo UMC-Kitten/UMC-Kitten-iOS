1391ef9091f05032b84d8668688c1318aceb4e77
leedoyeon849 <do83430208@gmail.com>
2024-02-11 15:50:58 +0900

    feat: Naver 로그인 관련 Service 추가

--- UMC-Kitten-iOS/Data/Service/Social/NaverAuthService.swift
+++ UMC-Kitten-iOS/Data/Service/Social/NaverAuthService.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import NaverThirdPartyLogin
+
+class NaverAuthService {
+    
+    func initializeNaverAuthSdk() {
+        
+        // 네이버 앱으로 로그인 허용
+        NaverThirdPartyLoginConnection.getSharedInstance()?.isNaverAppOauthEnable = true
+        // 브라우저 로그인 허용
+        NaverThirdPartyLoginConnection.getSharedInstance()?.isInAppOauthEnable = true
+        
+        // 네이버 로그인 세로모드 고정
+        NaverThirdPartyLoginConnection.getSharedInstance().setOnlyPortraitSupportInIphone(true)
+        
+        // NaverThirdPartyConstantsForApp.h에 선언한 상수 등록
+        NaverThirdPartyLoginConnection.getSharedInstance().serviceUrlScheme = kServiceAppUrlScheme
+        NaverThirdPartyLoginConnection.getSharedInstance().consumerKey = kConsumerKey
+        NaverThirdPartyLoginConnection.getSharedInstance().consumerSecret = kConsumerSecret
+        NaverThirdPartyLoginConnection.getSharedInstance().appName = kServiceAppName
+    }
+    
+    func openNaverLogin(completion: @escaping (String) -> Void) {
+        if NaverThirdPartyLoginConnection
+            .getSharedInstance()
+            .isPossibleToOpenNaverApp() // Naver App이 깔려있는지 확인
+        {
+            NaverThirdPartyLoginConnection
+                .getSharedInstance()
+                .requestThirdPartyLogin()
+        } else { // 네이버 앱 안깔려져 있을때
+            // Appstore에서 네이버앱 열기
+            NaverThirdPartyLoginConnection.getSharedInstance().openAppStoreForNaverApp()
+        }
+    }
+}
